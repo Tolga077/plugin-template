ed157d83564b859f4feb79cfa539add1dc68d7a9
KillerDogeEmpire <87155550+KillerDogeEmpire@users.noreply.github.com>
2022-09-25 23:54:07 -0700

    Start Animixplay provider

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -4,21 +4,24 @@ version = 1
 
 cloudstream {
     // All of these properties are optional, you can safely remove them
+    language = "en"
 
     description = "Lorem Ipsum"
-    authors = listOf("Cloudburst")
+    authors = listOf("Sir Aguacata (Aka KillerDogeEmpire)")
 
     /**
-    * Status int as the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta only
-    * */
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
     status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "Anime",
+        "AnimeMovie",
+        "OVA",
+    )
 
-    // List of video source types. Users are able to filter for extensions in a given category.
-    // You can find a list of avaliable types here:
-    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
-    tvTypes = listOf("Others")
+    iconUrl = "https://www.google.com/s2/favicons?domain=animixplay.to&sz=%size%"
 }
--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -4,15 +4,19 @@ import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.SearchResponse
 
-class ExampleProvider : MainAPI() { // all providers must be an instance of MainAPI
-    override var mainUrl = "https://example.com/" 
-    override var name = "Example provider"
-    override val supportedTypes = setOf(TvType.Movie)
 
-    override var lang = "en"
+class AnimixplayProvider: MainAPI() {  // all providers must be an instance of MainAPI
 
+    override var name = "Animixplay"
+    override var mainUrl = "https://animixplay.to"
     // enable this when your provider has a main page
     override val hasMainPage = true
+    override val hasChromecastSupport = true
+    override val hasDownloadSupport = true
+    override val supportedTypes = setOf(TvType.Anime)
+    override val hasQuickSearch = true
+
+    override var lang = "en"
 
     // this function gets called when you search for something
     override suspend fun search(query: String): List<SearchResponse> {
