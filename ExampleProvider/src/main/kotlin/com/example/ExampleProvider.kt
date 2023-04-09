4b36b9402d0fef07f0654ffdf8848f947243602b
Tolga077 <105724628+Tolga077@users.noreply.github.com>
2023-04-09 19:26:15 +0300

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,21 +1,13 @@
-package com.example
+package com.example.dizilla2
 
-import com.lagradost.cloudstream3.TvType
-import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
 
-class ExampleProvider : MainAPI() { // all providers must be an instance of MainAPI
-    override var mainUrl = "https://example.com/" 
-    override var name = "Example provider"
-    override val supportedTypes = setOf(TvType.Movie)
-
-    override var lang = "en"
-
-    // enable this when your provider has a main page
-    override val hasMainPage = true
-
-    // this function gets called when you search for something
-    override suspend fun search(query: String): List<SearchResponse> {
-        return listOf<SearchResponse>()
-    }
+@CloudstreamPlugin
+class Dizilla2Plugin: Plugin() {
+override fun load(context: Context) {
+// Tüm sağlayıcılar bu şekilde eklenmelidir. Lütfen sağlayıcılar listesini doğrudan düzenlemeyin.
+registerMainAPI(Dizilla2())
+}
 }
